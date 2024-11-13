47365931c3c53026651450b8c2c28c05068a8163
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-11-12 23:34:08 -0800

    update template

--- Sources/client.swift
+++ Sources/client.swift
@@ -6,13 +6,15 @@ actor CPU: Player {
     var myPlayerID: Int = -1
     var myCards: [Card] = []
     var sharedCards: [Card] = []
+    var money = 25 * 100 // $25 in cents
+    var pot = 0
 
     func cardsWereRevealed(event: CardReveal) {
         self.sharedCards.append(contentsOf: event.cards)
     }
 
     func turnDidOccur(event: Turn) {
-        
+        self.pot = event.potValue
     }
 
     func doTurn(event: TurnRequest) -> Action {
@@ -23,5 +25,7 @@ actor CPU: Player {
         self.myPlayerID = event.playerID
         self.myCards = event.cards
         self.sharedCards = []
+        self.money -= event.ante
+        self.pot = event.numberOfPlayers * event.ante
     }
 }
